d4570720d0ac0541ce176c951385833485eca073
Dylan Hoogduin <dylanhoogduin@Dylans-MacBook-Pro.local>
2020-08-13 12:50:16 +0200

    fix bug switching datasets

--- src/components/modules/ClusterViewer/index.js
+++ src/components/modules/ClusterViewer/index.js
@@ -4,8 +4,11 @@ import PageLoader from "../../hooks/pageLoader";
 import GeneExpressionForm from "./items/GeneExpressionForm";
 import {Dropdown, Grid, Header} from "semantic-ui-react";
 import {Form} from "semantic-ui-react";
+import Parser from "html-react-parser";
+import Iframe from "react-iframe";
 const GeneralCluser = React.lazy(() => import("./items/GeneralCluster"));
 const GeneExpressionTab = React.lazy(() => import("./items/GeneExpressionTab"));
+
 
 const initialState = {isLoading: false,
     value: '', selected: false, selectedGene: null,
@@ -17,13 +20,37 @@ class ClusterViewer extends Component {
     state = initialState;
 
     setDataset = (e, data) =>{
+        const index = _.map(this.props.collection, (item) => {return item.dataset.id}).indexOf(data.value)
+        console.log(index);
         this.setState({
             ds: data.value,
-            index: data.n
+            datasetIndex: index
         });
 
         this.props.setActiveDataset(data.value)
         this.props.getCells()
+    }
+    loadImage = (image, type) => {
+        if (!image)
+            return null;
+        return(
+            <>
+            <Grid.Row>
+                <Grid.Column>
+                    <Header as='h3' dividing>
+                        Image atlas of {type}
+                    </Header>
+                </Grid.Column>
+            </Grid.Row>
+            <Iframe url={image}
+                    allow={"fullscreen"}
+                    scrolling="no"
+                    className="iframe"
+                    display="initial"
+                    position="relative"
+            />
+            </>
+        )
     }
     componentDidMount() {
         const activeDs = this.props.collection[0].dataset;
@@ -42,6 +69,7 @@ class ClusterViewer extends Component {
     }
     render() {
         if (this.props.modulesData.cells.length < 1 ) return null
+        const activeSet = this.props.collection[this.state.datasetIndex]
         return (
             <>
                 <Grid.Row centered>
@@ -57,11 +85,28 @@ class ClusterViewer extends Component {
                 </Form>
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row >
-                    <Grid.Column >
+                <GeneExpressionForm
+                    getCellsByGene={this.props.getCellsByGene}
+                    getGeneSuggestions={this.props.getGeneSuggestions}
+                    modulesData={this.props.modulesData}
+                    resetCellByGene={this.props.resetCellByGene}
+                />
+                <Grid.Row>
+                    <Grid.Column>
                         <Header as='h3' dividing>
-                            {this.props.collection[this.state.datasetIndex].type}
+                            {activeSet.type}
                         </Header>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered>
+                    <Grid.Column width={8}>
+
+
+                        <p>
+                            <b>Authors</b>{Parser(activeSet.citation)}
+                            <b>Sequencing platform</b> {Parser(activeSet.data_origin)}
+                            <b>Data availability </b> {Parser(activeSet.geo)}
+                        </p>
                     </Grid.Column>
                 </Grid.Row>
                 <Suspense fallback={<PageLoader frame={true}/>}>
@@ -71,12 +116,6 @@ class ClusterViewer extends Component {
                         modulesData={this.props.modulesData}
                     />
                 </Suspense>
-                <GeneExpressionForm
-                    getCellsByGene={this.props.getCellsByGene}
-                    getGeneSuggestions={this.props.getGeneSuggestions}
-                    modulesData={this.props.modulesData}
-                    resetCellByGene={this.props.resetCellByGene}
-                />
                 <Suspense fallback={
                     (!_.isEmpty(this.props.modulesData.cellsByGene))?<PageLoader frame={true}/>:null
                 }>
@@ -86,6 +125,7 @@ class ClusterViewer extends Component {
                         modulesData={this.props.modulesData}
                     />
                 </Suspense>
+                {this.loadImage(activeSet.img_url, activeSet.type)}
             </>
         )
     }
